dfa46b85a22a6a87c416a0b6157de250e1799bb0
alucas5420 <112018521+alucas5420@users.noreply.github.com>
2023-11-04 16:31:23 -0400

    Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,24 @@
 import React from "react";
 import styles from "@/styles/Home.module.css";
 import Hero from "@/components/Hero";
+import Nav from "@/components/Nav";
+import Line from "@/components/Line";
 
 export default function Home() {
   return (
     <>
-    <Hero heroImage="/hero.jpg" height="200px" header="Main Hero" />
-    <h1 className={styles.bigRedFont}>Home</h1>
-    <p>This is the landing page for our Next.js app.</p>
-    <p className={styles.blueFont}>This should be blue font.</p>
-    <Hero heroImage="/hero.jpg" height="600px" />
+    <Nav header = "Home " header2 = " About " header3 = "  Products " header4 = "  Contact"/>
+    
+    <Hero heroImage="/TheCity.jpg" height="400px" header="IUPUI" />
+    <Line/>
+    <br></br> 
+    <h1 className={styles.iupuiFont}>Welcome to IUPUI!</h1>
+    <p className={styles.otherFont}>This is the home page for the IUPUI official site.</p>
+    <p className={styles.otherFont}>Be sure to look around.</p>
+    <Line/>
+    <Hero heroImage="/city.jpg" height="500px" />
+
+    <Line/>
     
     </>
   )
